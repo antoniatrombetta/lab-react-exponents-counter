4832039081365022ce355d717f65bdc586766049
antonia trombetta <antonia.dorazio@gmail.com>
2024-08-09 15:35:15 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+
+
 import "./App.css";
 import Counter from "./components/Counter";
 import ExponentTwo from "./components/ExponentTwo";
@@ -8,22 +11,30 @@ import ExponentSix from "./components/ExponentSix";
 
 
 function App () {
+
+  const [count, setCount] = useState(0);
+  
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
+  
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} callbackIncrement={increment} callbackDecrement={decrement} />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
-      </div>
+        <ExponentTwo count={count}  />
+        <ExponentThree count={count}   />
+        <ExponentFour count={count}  />
+        <ExponentFive count={count}  />
+        <ExponentSix count={count}  />
+      </div> 
     </div>
   );
 }
